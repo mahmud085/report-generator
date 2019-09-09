cb7b1f13562530a26dd96a7a12bf34766e6077c6
Josua Decker <josua.decker@mediatain.com>
2019-09-09 11:35:37 +0200

    FIx typo in test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,6 @@
 import index from './index';
 
-it('should return string `Hello world', () => {
+it('should return string `Hello world`', () => {
   const result = index();
 
   expect(result).toBe('Hello world');
