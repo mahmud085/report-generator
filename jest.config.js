957fd30cc7caf12d1f32835a6f504a17108864fe
Josua Decker <josua.decker@mediatain.com>
2019-09-09 11:35:24 +0200

    Update coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -17,10 +17,16 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 0,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+    'src/**/*.ts': {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
     },
   },
 };
